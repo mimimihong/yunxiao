e7b030874a58f43f93db6630ec770b37a4f73e62
mimimihong <151962205+mimimihong@users.noreply.github.com>
2024-02-23 10:46:40 +0800

    学生信息界面

--- 前端/云顶书院学生管理系统/管理端登录/管理信息界面/学生信息页面/学生信息页面.js
+++ 前端/云顶书院学生管理系统/管理端登录/管理信息界面/学生信息页面/学生信息页面.js
@@ -1,1 +1,37 @@
+data = [
+    { name: "张三", stuId: "12345678", direction: "全栈", period: "八期" }, { name: "李四", stuId: "56789101", direction: "全栈", period: "八期" }, { name: "王五", stuId: "87654321", direction: "全栈", period: "八期" }
+]
 var all = document.getElementsByClassName("all");
+let i = 0;
+for (i = 0; i < data.length; i++) {
+    var div = document.createElement("div");
+    div.innerHTML = "姓名:" + data[i].name + "&nbsp&nbsp&nbsp&nbsp学号:" + data[i].stuId + "&nbsp&nbsp&nbsp&nbsp方向:" + data[i].direction + "&nbsp&nbsp&nbsp&nbsp期数:" + data[i].period;
+    all[0].appendChild(div);
+}
+
+var submmit = document.getElementById("submmit");
+console.dir(submmit);
+submmit.onclick = function () {
+    var search = document.getElementById("search");
+    var stuId = search.value;
+    console.log(stuId);
+    all[0].innerHTML = "";
+    for (i = 0; i < data.length; i++) {
+        if (data[i].stuId == stuId) {
+            var div = document.createElement("div");
+            div.innerHTML = "姓名:" + data[i].name + "&nbsp&nbsp&nbsp&nbsp学号:" + data[i].stuId + "&nbsp&nbsp&nbsp&nbsp方向:" + data[i].direction + "&nbsp&nbsp&nbsp&nbsp期数:" + data[i].period;
+            all[0].appendChild(div);
+            i = data.length;
+        }
+        if (i == data.length - 1) {
+            var div = document.createElement("div");
+            div.innerHTML = "查无此人";
+            all[0].appendChild(div);
+
+        }
+    }
+}
+var back = document.getElementsByClassName("back");
+back[0].onclick = function () {
+    location.href = "../管理信息界面.html";
+}
