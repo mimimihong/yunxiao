aa0731061f659c8bebd468d63c22c3432f125978
mimimihong <151962205+mimimihong@users.noreply.github.com>
2024-03-05 23:13:29 +0800

    a'a'aaaa

--- 前端/fvAuth-app/src/test/java/com/fvauth/fvauthapp/inter/FunRead.java
+++
@@ -1,8 +0,0 @@
-package com.fvauth.fvauthapp.inter;
-
-import com.fvauth.fvauthapp.service.ReadObjFormat;
-
-public interface FunRead {
-
-    ReadObjFormat startInit(ReadObjFormat readObjFormat, String abc);
-}
